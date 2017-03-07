c571191158da16bb13f39d4c8aa6d9cf708bf9a4
Brandon G <brandongob@gmail.com>
2017-03-07 12:37:59 -0700

    Initial Project

---
+++ RealisticEvolution/Assets/Organism.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organism : MonoBehaviour {
+    public float Energy = 100f;
+    public float Efficency = .25f;
+    public int[] FoodSources = null; 
+    public int[] Wastes = null;
+    public int MaturityAge;
+    public int RefractoryPeriod;
+    public int ReproductionPeriod;
+
+    public float Strength;
+    public float Defense;
+    public float Health;
+
+    public bool food;
+
+
+
+
+    public bool EnableDebug = false;
+    public float ScalingProbability;
+    public string Name;
+    public GameObject World;
+    private Rigidbody rb;
+    // Use this for initialization
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        gameObject.name = Name;
+        food = false;
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!food)
+            Move();
+    }
+
+    public void Move()
+    {
+        float x, y, z;
+        if (Random.Range(0f, 100f) < ScalingProbability)
+        {
+            ScalingProbability = 5f;
+            if (Random.Range(0, 2) == 0)
+                x = Random.Range(0f, 180f);
+            else
+                x = 0;
+            if (Random.Range(0, 2) == 0)
+                y = Random.Range(0f, 180f);
+            else
+                y = 0;
+            if (Random.Range(0, 2) == 0)
+                z = Random.Range(0f, 180f);
+            else
+                z = 0;
+            transform.Rotate(0, y, 0);
+
+        }
+        else
+        {
+            // if the snitch hits a wall then its velocity should end up at 0, this ensures that if that happens, the snitch
+            // will react and turn around instead of sitting there waiting for a random rotation away from the wall
+            if (rb.velocity.magnitude > 0)
+            {
+                ScalingProbability += (4f / 4f) * (1 / rb.velocity.magnitude);
+            }
+            else
+            {
+                ScalingProbability = 100f;
+            }
+        }
+        
+        rb.AddForce(transform.forward * 5, ForceMode.Acceleration);
+        Energy -= 0.25f;
+        if (EnableDebug)
+        {
+            Debug.Log("Energy remaining ");
+            Debug.Log(Energy);
+            Debug.Log("-------------");
+        }
+    }
+
+
+    void OnCollisionEnter(Collision call)
+    {
+        
+    }
+
+    public void Look()
+    {
+
+    }
+}
---
+++ RealisticEvolution/Assets/World.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+    public Organism org;
+    public Organism basicFood;
+    private GameObject ingameTerrain;
+    private TerrainData data;
+    public Vector3 position = new Vector3(0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+        GameObject terrain = new GameObject();
+        data = new TerrainData();
+        terrain = Terrain.CreateTerrainGameObject(data);
+        terrain.name = "Custom Map igguh";
+        SetupMap(2f);
+
+        Organism orgs;
+        Vector3 spawnLocation = Vector3.zero;
+        Quaternion initialRotation = Quaternion.Euler(Vector3.zero);
+
+        spawnLocation.y = org.transform.localScale.y / 2;
+
+        for (int i = 0; i < 2; i++)
+        {
+            spawnLocation.x += 5f;
+
+            float r = Random.Range(0f, 1f);
+            float g = Random.Range(0f, 1f);
+            float b = Random.Range(0f, 1f);
+
+            Color speciesColor = new Color(r, g, b);
+
+            for (int j = 0; j < 2; j++)
+            {
+                spawnLocation.z += 5f;
+                orgs = Instantiate<Organism>(org, spawnLocation, initialRotation);
+
+                //speciesColor.r += Random.Range(-0.2f, 0.2f);
+                //speciesColor.g += Random.Range(-0.2f, 0.2f);
+                //speciesColor.b += Random.Range(-0.2f, 0.2f);
+                orgs.ReproductionPeriod = 100 + Random.Range(-10, 10);
+                orgs.RefractoryPeriod = 100 + Random.Range(-10, 10);
+                orgs.Efficency = 100 + Random.Range(-10, 10);
+                orgs.MaturityAge = 100 + Random.Range(-10, 10);
+                orgs.Health = 100 + Random.Range(-10, 10);
+                orgs.Strength = 100 + Random.Range(-10, 10);
+                orgs.Defense = 100 + Random.Range(-10, 10);
+                orgs.GetComponent<Renderer>().material.SetColor("_Color", speciesColor);
+            }
+        }
+        GameObject.Destroy(org.gameObject);
+        spawnLocation.y = basicFood.transform.localScale.y / 2;
+        for (int i = 0; i < 2; i++)
+        {
+            spawnLocation.x = Random.Range(0f, 10f);
+            spawnLocation.z = Random.Range(0f, 10f);
+            orgs = Instantiate<Organism>(basicFood, spawnLocation, initialRotation);
+            orgs.Energy = Random.Range(1f, 20f);
+            orgs.food = true;
+        }
+
+        GameObject.Destroy(basicFood.gameObject);
+
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+    public void SetupMap(float depth)
+    {
+        float xWidth = data.size.x;
+        float yWidth = data.size.z;
+        float[,] heights = new float[(int)xWidth, (int)yWidth];
+
+        for (int j = 0; j < yWidth; j++)
+        {
+            heights[0, j] = 3f;
+            heights[(int)(yWidth - 1), j] = 3f;
+        }
+
+        for (int j = 0; j < xWidth; j++)
+        {
+            heights[j, 0] = 3f;
+            heights[j, (int)(xWidth - 1)] = 3f;
+        }
+
+        data.SetHeights(1, 1, heights);
+
+        print(data.size);
+    }
+}
